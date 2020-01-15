71c8f4c0255dda2b39f811da8f845bb5ef3442ee
lijiayu <jy123456789>
2020-01-15 17:07:20 +0800

    Initial commit

---
+++ src/test/java/com/turtle/tblog/TblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turtle.tblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
