017871f8702c333623e9883329a7d9faf400c000
lijiayu <jy123456789>
2020-01-17 17:45:08 +0800

    <feat>整合Redis和Security。

--- web/src/main/java/com/turtle/web/WebApplication.java
+++ web/src/main/java/com/turtle/web/WebApplication.java
@@ -6,14 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.turtle.web","com.turtle.common"})
 @MapperScan("com.turtle.web.mapper")
-@ComponentScan(basePackages = {
-        "com.turtle.common.config",
-        "com.turtle.web.controller",
-        "com.turtle.web.service",
-        "com.turtle.common.handler"
-                                })
+//@ComponentScan(basePackages = {
+//        "com.turtle.common.config",
+//        "com.turtle.web.controller",
+//        "com.turtle.web.service",
+//        "com.turtle.common.handler",
+//        "com.turtle.web.config"
+//                                })
 public class WebApplication {
 
     public static void main(String[] args) {
--- web/src/main/java/com/turtle/web/controller/UserController.java
+++ web/src/main/java/com/turtle/web/controller/UserController.java
@@ -4,6 +4,7 @@ import com.turtle.common.aop.TargetDataSource;
 import com.turtle.common.enums.ExceptionEnum;
 import com.turtle.common.exception.BizException;
 import com.turtle.common.exception.FrontRequestException;
+import com.turtle.common.util.RedisUtil;
 import com.turtle.web.entity.User;
 import com.turtle.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,15 +26,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisUtil redisUtil;
+
     @GetMapping("/test")
     public List<User> getUser(Integer type){
-        if(type==null){
-            throw new NullPointerException();
-        }else if(type==1){
-            throw new BizException("-1","自定义异常");
-        }else if(type==2){
-            throw new FrontRequestException("前端异常");
-        }
+        redisUtil.set("test","123");
         return userService.getUserList();
     }
 }
