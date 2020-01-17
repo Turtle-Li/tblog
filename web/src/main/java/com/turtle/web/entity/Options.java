eb9c1508b30f5b7e781e11c536d666e2e94a2166
lijiayu <jy123456789>
2020-01-17 18:37:19 +0800

    <feat>整合邮件和rabbitMq(未完成)。

---
+++ web/src/main/java/com/turtle/web/entity/Options.java
@@ -0,0 +1,19 @@
+package com.turtle.web.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * @author lijiayu
+ * @date 2020/1/17
+ * @description
+ */
+@Data
+@Accessors(chain = true)
+@TableName("tbg_options")
+public class Options {
+    private String id;
+    private String key;
+    private String value;
+}
