66846c77e3026aa8cad968594988536830feab09
付宗乐 <fuzongle@163.com>
2020-01-18 11:25:34 +0800

    FIX:网关注册中心集成中

--- gateway/src/main/java/com/turtle/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/turtle/gateway/GatewayApplication.java
@@ -1,13 +1,34 @@
 package com.turtle.gateway;
 
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-
+    //测试使用
+    @Bean
+    public BlockRequestHandler blockRequestHandler() {
+        return new BlockRequestHandler() {
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange serverWebExchange, Throwable throwable) {
+                return ServerResponse.status(500)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .body(BodyInserters.fromObject("当前人流量较大，您的请求被限流了"));
+            }
+        };
+    }
 }
