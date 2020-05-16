32f0f2f2b6c70df0c86baf4ffe21d264cb3e3481
fuzongle <fuzongle>
2020-05-16 21:17:52 +0800

    feat:测试是否正常提交

--- admin/src/main/java/com/turtle/controller/TestController.java
+++ admin/src/main/java/com/turtle/controller/TestController.java
@@ -1,15 +1,18 @@
 package com.turtle.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Auther: fuzongle
  * @Date: 2020/5/16 18:17
- * @Description:
+ * @Description:  测试过后删除
  */
 @RestController
+@RequestMapping("/user")
+@Api(tags = {"测试是否改变数据"})
 public class TestController {
-
 
 
     public String test(){
