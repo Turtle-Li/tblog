2cb9e7de4826069a24f47661ba1f45c82b3e82c1
lijiayu <jy123456789>
2020-05-20 15:04:58 +0800

    <feat> 线程池。

--- common/src/main/java/com/turtle/aop/ValidCode.java
+++ common/src/main/java/com/turtle/aop/ValidCode.java
@@ -1,10 +1,17 @@
 package com.turtle.aop;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author lijiayu
  * @date 2020/5/19
  * @description
  */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ValidCode {
 
 }
---
+++ common/src/main/java/com/turtle/config/pool/TaskPoolConfig.java
@@ -0,0 +1,30 @@
+package com.turtle.config.pool;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author lijiayu
+ * @date 2020/5/20
+ * @description
+ */
+@Configuration
+@EnableAsync
+public class TaskPoolConfig {
+    @Bean("taskExecutor")
+    public Executor taskExecutor(){
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(10);
+        taskExecutor.setMaxPoolSize(50);
+        taskExecutor.setQueueCapacity(200);
+        taskExecutor.setKeepAliveSeconds(60);
+        taskExecutor.setThreadNamePrefix("taskExecutor--");
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        taskExecutor.setAwaitTerminationSeconds(60);
+        return taskExecutor;
+    }
+}
