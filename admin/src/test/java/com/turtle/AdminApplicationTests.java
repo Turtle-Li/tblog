d94cdb094933b55f05328c5a5875c797105ccc11
lijiayu <jy123456789>
2020-05-27 15:36:24 +0800

    <feat> 测试自动打包。

--- admin/src/test/java/com/turtle/AdminApplicationTests.java
+++ admin/src/test/java/com/turtle/AdminApplicationTests.java
@@ -1,13 +1,24 @@
 package com.turtle;
 
+import com.turtle.dto.AssignRoleParam;
+import com.turtle.service.ApiService;
+import com.turtle.service.LoginService;
+import com.turtle.service.RoleService;
+import com.turtle.util.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 @SpringBootTest
 @Slf4j
 class AdminApplicationTests {
 
-   /* @Autowired
+    @Autowired
     private LoginService loginService;
     @Autowired
     private ApiService apiService;
@@ -22,11 +33,12 @@ class AdminApplicationTests {
 
     @Test
     void list(){
-        roleService.assign(new AssignRoleParam().setUserId(1259826206840233986L).setRoleIds(Set.of(1259827052512587778L)));
+//        roleService.assign(new AssignRoleParam().setUserId(1259826206840233986L).setRoleIds(Set.of(1259827052512587778L)));
     }
 
     @Test
     void contextLoads() throws InterruptedException {
+//        CompletableFuture<String> c1 = CompletableFuture.supplyAsync(() -> "aaaa");
+        System.out.println("111");
     }
-*/
 }
